64f8756aa5d4d5343ed0e13168761446d8958e2a
Joko Santoso <joko74479@gmail.com>
2023-12-20 12:22:30 +0700

    add: feat:delete, updateStatus, filter

--- js/script.js
+++ js/script.js
@@ -1,46 +1,13 @@
-// const containerTask = document.querySelector(".container-task");
-// const btnAdd = document.querySelector(".btn-add");
-
-// document.addEventListener("keyup", function (event) {
-// 	if (event.key === "Enter") {
-// 		const input = document.getElementById("text");
-// 		if (input.value.length > 0) {
-// 			containerTask.innerHTML += getTask(input.value);
-
-// 			input.value = "";
-// 		} else {
-// 			input.classList.add("eror");
-// 			setTimeout(function () {
-// 				input.classList.remove("eror");
-// 			}, 600);
-// 		}
-// 	}
-// });
-
-// document.addEventListener("click", function (e) {
-// 	if (e.target.classList.contains("delete-icon")) {
-// 		e.target.parentElement.parentElement.style.display = "none";
-// 	}
-// });
-
-// function getTask(n) {
-// 	return /*html*/ `
-// 				<li class="section-taks">
-// 					<label for="1" class="checkbox-wrapper-11">
-// 						<input id="1" type="checkbox" name="r" value="2" />
-// 						<span class="task-content">${n}</span>
-// 					</label>
-// 					<div class="delete">
-//             <i class="uil uil-trash delete-icon"></i>
-// 					</div>
-// 				</li>`;
-// }
-
 const taskInput = document.querySelector("#text"),
-	taskBox = document.querySelector(".container-task");
+	taskBox = document.querySelector(".container-task"),
+	filterSpan = document.querySelectorAll(".filters span"),
+	clearAll = document.querySelector(".btn-clear");
 
 //! menyimpan hasil input user dilocalstorage berupa JSON
 let todos = JSON.parse(localStorage.getItem("todo-list"));
+if (!todos) {
+	todos = [];
+}
 
 // event ketika user input
 taskInput.addEventListener("keyup", function (event) {
@@ -48,9 +15,6 @@ taskInput.addEventListener("keyup", function (event) {
 		// input value
 		let usertask = taskInput.value;
 		if (taskInput.value.length > 0) {
-			if (!todos) {
-				todos = [];
-			}
 			taskInput.value = "";
 			let taskInfo = {
 				name: usertask,
@@ -58,7 +22,7 @@ taskInput.addEventListener("keyup", function (event) {
 			};
 			todos.push(taskInfo);
 			localStorage.setItem("todo-list", JSON.stringify(todos));
-			showTodo();
+			showTodo(document.querySelector("span.active").id);
 		} else {
 			taskInput.classList.add("eror");
 			setTimeout(() => {
@@ -68,20 +32,51 @@ taskInput.addEventListener("keyup", function (event) {
 	}
 });
 
-function showTodo() {
+filterSpan.forEach((btn) => {
+	btn.addEventListener("click", function () {
+		document.querySelector("span.active").classList.remove("active");
+		btn.classList.add("active");
+		showTodo(btn.id);
+	});
+});
+
+clearAll.addEventListener("click", () => {
+	todos.splice(0, todos.length);
+	localStorage.setItem("todo-list", JSON.stringify(todos));
+	showTodo();
+});
+
+function showTodo(filter) {
 	let litag = "";
 	if (todos) {
 		todos.forEach((todo, id) => {
-			litag += `<li class="section-taks">
+			if (filter == todo.status || filter == "all") {
+				litag += /*html*/ `<li class="section-taks">
 									<label for="${id}" class="checkbox-wrapper-11">
-										<input id="${id}" type="checkbox" name="r" value="2" />
+										<input onclick="updateStatus(this)" id="${id}" type="checkbox" name="r" value="2" ${
+					todo.status === "completed" ? "checked" : ""
+				} />
 										<span class="task-content">${todo.name}</span>
 									</label>
-									<div class="delete">
+									<div class="delete" onclick="deleteTask(${id}, ${filter})">
 										<i class="uil uil-trash delete-icon"></i>
 									</div>
 								</li>`;
+			}
 		});
 	}
-	taskBox.innerHTML = litag;
+	taskBox.innerHTML = litag || `<span class="clear">You don't have any task here</span>`;
+}
+showTodo("all");
+
+function updateStatus(selectedTask) {
+	const taskId = selectedTask.id;
+	todos[taskId].status = selectedTask.checked ? "completed" : "pending";
+	localStorage.setItem("todo-list", JSON.stringify(todos));
+}
+
+function deleteTask(deleted, filter) {
+	todos.splice(deleted, 1);
+	localStorage.setItem("todo-list", JSON.stringify(todos));
+	showTodo(filter);
 }
