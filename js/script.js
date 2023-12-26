b2315bba9102e03b94dbf5c1a1138661a5c3e04c
Joko Santoso <joko74479@gmail.com>
2023-12-26 20:56:07 +0700

    fix: css.scrolY, js.editTask

--- js/script.js
+++ js/script.js
@@ -8,100 +8,7 @@ const taskInput = document.querySelector("#text"),
 let editId;
 let isEditTask = false;
 //! menyimpan hasil input user dilocalstorage berupa JSON
-let todos = JSON.parse(localStorage.getItem("todo-list"));
-todos = [
-	{
-		name: "Your List",
-		status: "pending",
-	},
-];
-
-// event ketika user input
-taskInput.addEventListener("keyup", function (event) {
-	if (event.key === "Enter") {
-		// input value
-		let usertask = taskInput.value;
-		if (taskInput.value.length > 0) {
-			// delete inpur user
-			taskInput.value = "";
-			// jika edit true
-			if (!isEditTask) {
-				// todo: todos
-				let taskInfo = {
-					name: usertask,
-					status: "pending",
-				};
-				todos.push(taskInfo);
-			} else {
-				// jika edit false
-				isEditTask = false;
-				todos[editId].name = usertask;
-			}
-			// set item in json localstorage
-			localStorage.setItem("todo-list", JSON.stringify(todos));
-			// function showtodo()
-			showTodo(document.querySelector("span.active").id);
-		} else {
-			// add class in input user
-			taskInput.classList.add("eror");
-			// remove class
-			setTimeout(() => {
-				this.classList.remove("eror");
-			}, 600);
-		}
-	}
-});
-
-// looping controls
-filterSpan.forEach((btn) => {
-	// eventlistener in every <span/>
-	btn.addEventListener("click", function () {
-		// delete class active in span
-		document.querySelector("span.active").classList.remove("active");
-		// add class active
-		btn.classList.add("active");
-		// show task => parameter btn.id in todos
-		showTodo(btn.id);
-	});
-});
-
-// delete task ALL
-clearAll.addEventListener("click", () => {
-	// hapus semua task
-	todos.splice(0, todos.length);
-
-	// set item json localstorage
-	localStorage.setItem("todo-list", JSON.stringify(todos));
-	showTodo();
-});
-
-// function update status in json localstorage
-function updateStatus(selectedTask) {
-	// var input.id
-	const taskId = selectedTask.id;
-	// every click/check => status->complate
-	todos[taskId].status = selectedTask.checked ? "completed" : "pending";
-	localStorage.setItem("todo-list", JSON.stringify(todos));
-}
-
-// function delete task
-function deleteTask(deleted) {
-	todos.splice(deleted, 1);
-	localStorage.setItem("todo-list", JSON.stringify(todos));
-	showTodo(document.querySelector("span.active").id);
-}
-
-function editTask(taskId, taskName) {
-	/*
-		output: index of todos, name of task eventClick
-	*/
-	isEditTask = true;
-	editId = taskId;
-	// input user
-	taskInput.value = taskName;
-	taskInput.focus();
-	showTodo("all");
-}
+let todos = JSON.parse(localStorage.getItem("todo-list")) || [];
 
 // show task in client
 function showTodo(filter) {
@@ -117,10 +24,12 @@ function showTodo(filter) {
 										<span class="task-content">${todo.name}</span>
 									</label>
 									<div class="settings" >
-										<div onclick="editTask(${id}, '${todo.name}')" class="edit" >
+										<div onclick="editTask(${id}, '${todo.name}')" class="edit" ${
+					todo.status === "completed" ? 'style="visibility: hidden"' : ""
+				}  >
 											<i class="uil uil-pen"></i>
 										</div>
-										<div onclick="deleteTask(${id})" class="delete">
+										<div onclick="deleteTask(${id}, '${filter}')" class="delete">
 											<i class="uil uil-trash delete-icon"></i>
 										</div>
 									</div>
@@ -129,9 +38,89 @@ function showTodo(filter) {
 		});
 		taskBox.innerHTML = litag || `<span class="clear">You don't have any task here</span>`;
 		// for scroll-Y of container-task if height > 202.67px
-		taskBox.offsetHeight > 204
-			? taskBox.classList.add("overflow")
-			: taskBox.classList.remove("overflow");
+		taskBox.classList.toggle("overflow", taskBox.offsetHeight > 204);
 	}
 }
 showTodo("all");
+
+// event ketika user input
+taskInput.addEventListener("keyup", function (event) {
+	if (event.key === "Enter") {
+		let usertask = taskInput.value.trim();
+		if (taskInput.value.length > 0) {
+			taskInput.value = "";
+			// editTask = true ? jika false makan akan menambah list task
+			if (!isEditTask) {
+				// todo: todos
+				let taskInfo = {
+					name: usertask,
+					status: "pending",
+				};
+				todos.push(taskInfo);
+			} else {
+				isEditTask = false;
+				todos[editId].name = usertask;
+			}
+
+			localStorage.setItem("todo-list", JSON.stringify(todos));
+			showTodo(document.querySelector("span.active").id);
+		} else {
+			taskInput.classList.add("eror");
+			setTimeout(() => {
+				this.classList.remove("eror");
+			}, 600);
+		}
+	}
+});
+
+// looping controls
+filterSpan.forEach((btn) => {
+	// eventlistener in every <span/>
+	btn.addEventListener("click", function () {
+		document.querySelector("span.active").classList.remove("active");
+		btn.classList.add("active");
+		showTodo(btn.id);
+	});
+});
+
+// delete task ALL
+clearAll.addEventListener("click", () => {
+	isEditTask = false;
+	todos.splice(0, todos.length);
+	localStorage.setItem("todo-list", JSON.stringify(todos));
+	showTodo();
+});
+
+// function update status in json localstorage
+function updateStatus(selectedTask) {
+	// var input.id
+	const taskId = selectedTask.id;
+	todos[taskId].status = selectedTask.checked ? "completed" : "pending";
+	const setting = document.querySelectorAll(".edit")[taskId];
+	if (todos[taskId].status == "completed") {
+		setting.style.visibility = "hidden";
+	} else {
+		setting.style.visibility = "visible";
+	}
+	localStorage.setItem("todo-list", JSON.stringify(todos));
+}
+
+// function delete task
+function deleteTask(deleted, filter) {
+	isEditTask = false;
+	todos.splice(deleted, 1);
+	localStorage.setItem("todo-list", JSON.stringify(todos));
+	showTodo(filter);
+}
+
+function editTask(taskId, taskName) {
+	/*
+		output: index of todos, name of task eventClick
+	*/
+	isEditTask = true;
+	editId = taskId;
+	// input user
+	taskInput.value = taskName;
+	taskInput.focus();
+	showTodo(document.querySelector("span.active").id);
+}
